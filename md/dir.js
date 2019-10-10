3d9ed8daacbf44383d0eb3ab00a2017a0a6dddab
chenjielang <KiriJayla@gmail.com>
2019-10-10 17:07:51 +0800

    [update]github.io update

---
+++ md/dir.js
@@ -0,0 +1,22 @@
+{
+    "dir": [
+        {
+            "name": "工作",
+            "dir" : [
+                {
+                    "name" : "7724-witgame-pc",
+                    "url" : "/md/1.md"
+                }
+            ]
+        },
+        {
+            "name" : "vue",
+            "dir" : [
+                {
+                    "name" : "test",
+                    "url": "/md/2.md"
+                }
+            ]
+        }
+    ]
+}
