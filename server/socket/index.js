1cb4bd70e52c20620fdf0e38cbad01284dc81ab9
chenjielang <541444869@qq.com>
2020-10-23 14:35:13 +0800

    test

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,5 +1,4 @@
-const os = require('os')
-const spawn = require('child_process').spawn
+const spawn = require('child_process').spawnSync
 const WebSocketServer = require('ws').Server
 
 module.exports = (server) => {
@@ -9,28 +8,21 @@ module.exports = (server) => {
             //打印客户端监听的消息
             console.log(message);
         });
-        ws.send(os.platform())
-        if (os.platform() == 'linux') {
-            let prc = spawn('free', [])
-            prc.stdout.setEncoding('utf8');
-            prc.stdout.on('data', function (data) {
+        if (process.platform == 'linux') {
+            let timer = setInterval(() => {
+                let prc = spawn('free', [])
+                let data = prc.stdout
                 let str = data.toString()
                 let lines = str.split(/\n/g);
                 for (var i = 0; i < lines.length; i++) {
                     lines[i] = lines[i].split(/\s+/);
                 }
                 ws.send(JSON.stringify({
-                    data,
                     lines
                 }))
-                setTimeout(function() {
-                    prc = spawn('free', [])
-                }, 2000)
-            });
+            }, 2000)
             wss.on('close', function () {
-                prc.on('close', function (code) {
-                    console.log('process exit code ' + code);
-                });
+                clearInterval(timer)
             })
         }
 
