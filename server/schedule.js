5b58630cec331a836add07cc494d4d73185121cc
chenjielang <541444869@qq.com>
2021-04-13 21:04:46 +0800

    update

--- server/schedule.js
+++ server/schedule.js
@@ -2,11 +2,16 @@ const schedule = require('node-schedule');
 const fs = require('fs')
 const path = require('path')
 function clean() {
-    let list = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../upload/delete_list.json'), 'utf8'))
-    list.forEach(item => {
-        fs.unlinkSync(path.resolve(__dirname, '../static' + item))
-    })
-    fs.writeFileSync(path.resolve(__dirname, '../upload/delete_list.json'), JSON.stringify([]))
+    try {
+        let list = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../upload/delete_list.json'), 'utf8'))
+        list.forEach(item => {
+            fs.unlinkSync(path.resolve(__dirname, '../static' + item))
+        })
+        fs.writeFileSync(path.resolve(__dirname, '../upload/delete_list.json'), JSON.stringify([]))
+    } catch(e) {
+        console.err(e)
+    }
+
 }
 const  scheduleCronstyle = ()=>{
     // 每小时0分0秒清除资源
